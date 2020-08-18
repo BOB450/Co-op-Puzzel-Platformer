915614fb580c2e6613ce2d8ab4f2a88ea3571339
BOB450 <bwvoskuil@gmail.com>
2020-08-17 20:59:30 -0600

    Uploaded Unity  project

---
+++ Rigidbody2D Slopes Final/Assets/Scripts/Buttonscript.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using UnityEngine;
+
+
+
+public class Buttonscript : MonoBehaviour
+{
+
+    bool buttonpressed = false;//is button pressed
+    public Transform walldownpos;
+    public Transform walluppos;
+    public GameObject bluewall;
+    
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+    // player colides with button
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+
+        //if (myTrigger.gameObject.name == "Dana")
+      //  {
+           // Debug.Log("Entered");
+        buttonpressed = true;
+        isbuttonpressed();
+        //}
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        buttonpressed = false;
+        isbuttonpressed();
+    }
+
+    void isbuttonpressed()
+    {
+        if (buttonpressed == true)
+        {
+            Debug.Log("yes");
+
+            // DestroyGameObject();
+            movedown();
+
+
+        }
+        else
+        {
+            Debug.Log("no");
+            moveup();
+           
+
+        }
+
+        
+    }
+
+  
+
+    void movedown()
+    {
+        while (walldownpos.position.y < bluewall.transform.position.y)
+        {
+
+            bluewall.transform.Translate(0, -Time.deltaTime, 0, Space.World);
+            
+        }
+    }
+
+    void moveup()
+    {
+
+        while (walluppos.position.y > bluewall.transform.position.y)
+        {
+
+            bluewall.transform.Translate(0, Time.deltaTime, 0, Space.World);
+
+        }
+
+    }
+
+
+}
