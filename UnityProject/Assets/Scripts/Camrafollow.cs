2344065dba5436962b0be7c8134eddd702d56931
BOB450 <bwvoskuil@gmail.com>
2020-09-03 22:51:35 -0600

    Added camra follow script
    just drag script onto camra

---
+++ UnityProject/Assets/Scripts/Camrafollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Camrafollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position.x = new Vector3(); 
+        // target.transform.position = xcord;
+        //transform.position.y = target.transform.position.y;
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+    }
+}
