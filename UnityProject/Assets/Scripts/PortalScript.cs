5bec7474c87a0e55c0c5585facffddbc4834a808
BOB450 <bwvoskuil@gmail.com>
2020-09-15 11:39:15 -0600

    Added portals
    Made prefab

---
+++ UnityProject/Assets/Scripts/PortalScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    public Transform exPos;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        if (col.gameObject.tag == "bluePlayer" || col.gameObject.tag == "greenPlayer")
+        {
+            Player.transform.position = exPos.position;
+        }
+    }
+
+
+}
